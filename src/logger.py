9a9519b7ebdffcc423ce0daf0fafac0a1b4118f1
aaravmat1209 <aaravsmitmatalia@gmail.com>
2024-06-11 18:36:31 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from datetime import datetime
+
+# Naming convention for log files: YYYY-MM-DD.log
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d')}.log"
+
+# Create a custom logger
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+logging.basicConfig(
+  filename=LOG_FILE_PATH,
+  format='[%(asctime)s]%(lineno)d %(name)s - %(levelname)s: %(message)s', 
+  level=logging.INFO,
+)
+
+
+
